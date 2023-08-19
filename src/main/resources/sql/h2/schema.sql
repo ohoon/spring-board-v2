f81316f2009f9eabf6c25ada5fc15cb6ec30c34b
ohoon <0hoon5661@gmail.com>
2023-08-19 19:13:35 +0900

    h2 데이터베이스 설정

---
+++ src/main/resources/sql/h2/schema.sql
@@ -0,0 +1,37 @@
+CREATE TABLE member (
+    member_id          BIGINT,
+    username           VARCHAR(20)  NOT NULL,
+    nickname           VARCHAR(12)  NOT NULL,
+    email              VARCHAR(128) NOT NULL,
+    created_date       DATETIME     NOT NULL,
+    last_modified_date DATETIME     NOT NULL,
+    PRIMARY KEY (member_id)
+);
+
+CREATE TABLE post (
+    post_id            BIGINT,
+    member_id          BIGINT,
+    title              VARCHAR(64) NOT NULL,
+    content            TEXT        NOT NULL,
+    author             VARCHAR(20) NOT NULL,
+    view               BIGINT      NOT NULL DEFAULT 0,
+    is_removed         BOOLEAN     NOT NULL,
+    created_date       DATETIME    NOT NULL,
+    last_modified_date DATETIME    NOT NULL,
+    PRIMARY KEY (post_id),
+    FOREIGN KEY (member_id) REFERENCES member (member_id)
+);
+
+CREATE TABLE comment (
+    comment_id         BIGINT,
+    member_id          BIGINT,
+    post_id            BIGINT,
+    content            VARCHAR(255) NOT NULL,
+    author             VARCHAR(20)  NOT NULL,
+    is_removed         BOOLEAN      NOT NULL,
+    created_date       DATETIME     NOT NULL,
+    last_modified_date DATETIME     NOT NULL,
+    PRIMARY KEY (comment_id),
+    FOREIGN KEY (member_id) REFERENCES member (member_id),
+    FOREIGN KEY (post_id) REFERENCES post (post_id)
+);
