d87e33fea35042ce7591a2c73559804419ba1e5a
ohoon <0hoon5661@gmail.com>
2023-08-29 16:26:21 +0900

    로그인 실패 메시지 추가

--- src/main/java/com/ohoon/board/app/dto/AuthPasswordLoginDto.java
+++ src/main/java/com/ohoon/board/app/dto/AuthPasswordLoginDto.java
@@ -1,5 +1,6 @@
 package com.ohoon.board.app.dto;
 
+import jakarta.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,7 +10,9 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class AuthPasswordLoginDto {
 
+    @NotEmpty(message = "아이디를 입력해주세요.")
     private String username;
 
+    @NotEmpty(message = "비밀번호를 입력해주세요.")
     private String password;
 }
