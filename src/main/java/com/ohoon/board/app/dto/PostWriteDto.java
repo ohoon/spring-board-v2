1c6dbafad9befc3f8c746199ed29cacb7cd1dba7
ohoon <0hoon5661@gmail.com>
2023-08-31 18:04:35 +0900

    게시글 작성 기능 및 화면 구현

--- src/main/java/com/ohoon/board/app/dto/PostWriteDto.java
+++ src/main/java/com/ohoon/board/app/dto/PostWriteDto.java
@@ -2,6 +2,8 @@ package com.ohoon.board.app.dto;
 
 import com.ohoon.board.domain.Member;
 import com.ohoon.board.domain.Post;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,8 +13,12 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class PostWriteDto {
 
+    @NotEmpty(message = "제목을 입력해주세요.")
+    @Size(max = 64,
+            message = "제목은 64글자 이하 이어야 합니다.")
     private String title;
 
+    @NotEmpty(message = "내용을 입력해주세요.")
     private String content;
 
     public Post toEntity(Member member) {
--- src/main/java/com/ohoon/board/web/PostController.java
+++ src/main/java/com/ohoon/board/web/PostController.java
@@ -3,16 +3,18 @@ package com.ohoon.board.web;
 import com.ohoon.board.app.dto.CurrentMemberDto;
 import com.ohoon.board.app.dto.PostListDto;
 import com.ohoon.board.app.dto.PostReadDto;
+import com.ohoon.board.app.dto.PostWriteDto;
 import com.ohoon.board.app.security.CurrentMember;
 import com.ohoon.board.app.service.PostService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/post")
@@ -24,7 +26,8 @@ public class PostController {
     @GetMapping("")
     public String list(
             @CurrentMember CurrentMemberDto currentMember,
-            Model model) {
+            Model model
+    ) {
         Page<PostListDto> postListDtoPages = postService.list(PageRequest.of(0, 30));
         model.addAttribute("currentMember", currentMember);
         model.addAttribute("listDtos", postListDtoPages.getContent());
@@ -33,11 +36,38 @@ public class PostController {
         return "posts/list";
     }
 
+    @PreAuthorize("hasRole('MEMBER')")
+    @GetMapping("/write")
+    public String writeForm(
+            @CurrentMember CurrentMemberDto currentMember,
+            Model model
+    ) {
+        model.addAttribute("currentMember", currentMember);
+        model.addAttribute("writeDto", new PostWriteDto());
+        return "posts/writeForm";
+    }
+
+    @PreAuthorize("hasRole('MEMBER')")
+    @PostMapping("/write")
+    public String write(
+            @CurrentMember CurrentMemberDto currentMember,
+            @Valid @ModelAttribute("writeDto") PostWriteDto writeDto,
+            BindingResult result
+    ) {
+        if (result.hasErrors()) {
+            return "posts/writeForm";
+        }
+
+        postService.write(currentMember.getMemberId(), writeDto);
+        return "redirect:/post";
+    }
+
     @GetMapping("/{id}")
     public String read(
             @CurrentMember CurrentMemberDto currentMember,
             @PathVariable("id") Long postId,
-            Model model) {
+            Model model
+    ) {
         PostReadDto postReadDto = postService.read(postId);
         model.addAttribute("currentMember", currentMember);
         model.addAttribute("readDto", postReadDto);
