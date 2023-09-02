63ec9848758c8442c51a404f9ed5405165af34e4
ohoon <0hoon5661@gmail.com>
2023-09-02 18:17:12 +0900

    @EntityGraph로 fetch join하도록 수정

--- src/main/java/com/ohoon/board/app/repository/MemberRepository.java
+++ src/main/java/com/ohoon/board/app/repository/MemberRepository.java
@@ -1,7 +1,13 @@
 package com.ohoon.board.app.repository;
 
 import com.ohoon.board.domain.Member;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface MemberRepository extends JpaRepository<Member, Long>, MemberQueryRepository {
+import java.util.Optional;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    @EntityGraph(attributePaths = "roles")
+    Optional<Member> findByUsername(String username);
 }
